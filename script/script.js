fe5135fe8fb3d7ff94818147ebf37f9409b9286b
Your Name <micael.santos8@fatec.sp.gov.br>
2022-11-10 16:28:23 -0300

    adding choose length option

--- script/script.js
+++ script/script.js
@@ -1,13 +1,24 @@
-function generate(){
-     const myArray = new Uint32Array(1);
-     let text = "";
-     crypto.getRandomValues(myArray);
+const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@!#$%¨&*()+-*/+.,<>;/~][';
 
-     for (const num of myArray) {
-       text += num 
-     }
-     document.getElementById("password").value = text;
+function generate(length) {
+    var len = document.getElementById("len").value;
+    if (len < 1){
+     alert("please choose a length greater than 0")
     }
+    else{
+     let result = '';
+     const charactersLength = characters.length;
+     for (let i = 0; i < length; i++ ) {
+         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+     }
+
+     document.getElementById("password").value = result;
+     console.log(generate(len)); 
+    }
+      
+}
+
+    
 
 function copy(){
      var copyText = document.getElementById("password");
@@ -20,6 +31,8 @@ function copy(){
 
 
 }
+
+
 
         
    
