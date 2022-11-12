e5dcb7c8e671a43b4049c84f30946e439e8d565d
Your Name <micael.santos8@fatec.sp.gov.br>
2022-11-12 19:02:28 -0300

    finishing javascript code

--- script/script.js
+++ script/script.js
@@ -1,24 +1,83 @@
-const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@!#$%¨&*()+-*/+.,<>;/~][';
-
 function generate(length) {
     var len = document.getElementById("len").value;
+    numbersCheck = document.getElementById("numbers");
+    lettersCheck = document.getElementById("letters");
+    symbolsCheck = document.getElementById("symbols");
+
     if (len < 1){
      alert("please choose a length greater than 0")
     }
-    else{
+    else if (numbersCheck.checked && lettersCheck.checked == false && symbolsCheck.checked == false){
+     const characters ='0123456789';
      let result = '';
      const charactersLength = characters.length;
      for (let i = 0; i < length; i++ ) {
          result += characters.charAt(Math.floor(Math.random() * charactersLength));
      }
-
      document.getElementById("password").value = result;
      console.log(generate(len)); 
     }
-      
+    else if (lettersCheck.checked && numbersCheck.checked == false && symbolsCheck.checked == false){
+     const characters ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+     let result = '';
+     const charactersLength = characters.length;
+     for (let i = 0; i < length; i++ ) {
+         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+     }
+     document.getElementById("password").value = result;
+     console.log(generate(len)); 
+    }
+    else if (symbolsCheck.checked && lettersCheck.checked == false && numbersCheck.checked == false){
+     const characters ='!@#$%&*_-.';
+     let result = '';
+     const charactersLength = characters.length;
+     for (let i = 0; i < length; i++ ) {
+         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+     }
+     document.getElementById("password").value = result;
+     console.log(generate(len)); 
+    }
+    else if (lettersCheck.checked && numbersCheck.checked && symbolsCheck.checked == false){
+     const characters ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+     let result = '';
+     const charactersLength = characters.length;
+     for (let i = 0; i < length; i++ ) {
+         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+     }
+     document.getElementById("password").value = result;
+     console.log(generate(len)); 
+    }
+    else if (lettersCheck.checked && numbersCheck.checked && symbolsCheck.checked){
+     const characters ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%&*_-.';
+     let result = '';
+     const charactersLength = characters.length;
+     for (let i = 0; i < length; i++ ) {
+         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+     }
+     document.getElementById("password").value = result;
+     console.log(generate(len)); 
+    }
+    else if (lettersCheck.checked && numbersCheck.checked == false && symbolsCheck.checked){
+     const characters ='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%&*_-.';
+     let result = '';
+     const charactersLength = characters.length;
+     for (let i = 0; i < length; i++ ) {
+         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+     }
+     document.getElementById("password").value = result;
+     console.log(generate(len)); 
+    }
+    else if (lettersCheck.checked ==false && numbersCheck.checked && symbolsCheck.checked){
+     const characters ='0123456789!@#$%&*_-.';
+     let result = '';
+     const charactersLength = characters.length;
+     for (let i = 0; i < length; i++ ) {
+         result += characters.charAt(Math.floor(Math.random() * charactersLength));
+     }
+     document.getElementById("password").value = result;
+     console.log(generate(len)); 
+    }
 }
-
-    
 
 function copy(){
      var copyText = document.getElementById("password");
@@ -27,9 +86,6 @@ function copy(){
      copyText.setSelectionRange(0,99999);
 
      navigator.clipboard.writeText(copyText.value);
-
-
-
 }
 
 
